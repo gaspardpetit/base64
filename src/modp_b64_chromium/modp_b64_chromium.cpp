ab2eea2f419bbf44e229f3ad853baa31b7dc0c91
Gaspard Petit <gpetit@eidosmontreal.com>
2020-12-13 21:47:07 -0500

    Added chromium_modp_b64 to msvc project

--- src/modp_b64_chromium/modp_b64_chromium.cpp
+++ src/modp_b64_chromium/modp_b64_chromium.cpp
@@ -8,17 +8,55 @@ using namespace std;
 
 struct B64_Adapt
 {
-	static constexpr const size_t GetEncodedLengthWithoutPadding(size_t decodedLength)
+	static constexpr size_t GetEncodedLengthWithoutPadding(size_t decodedLength)
 	{
 		return (decodedLength * 8 + 5) / 6;
 	}
 
+	static constexpr size_t GetEncodedLengthWithPadding(size_t decodedLength)
+	{
+		return ((GetEncodedLengthWithoutPadding(decodedLength) + 3) / 4) * 4;
+	}
+
+	static size_t CountPadding(const std::string &encoded)
+	{
+		size_t padding = 0;
+		size_t len = encoded.length();
+		if (len == 0)
+			return 0;
+
+		if (encoded[len - 1] == '=')
+			++padding;
+
+		if (len == 1)
+			return padding;
+
+		if (encoded[len - 2] == '=')
+			++padding;
+
+		return padding;
+	}
+
+	static size_t GetDecodedLength(const std::string &encoded)
+	{
+		size_t padding = CountPadding(encoded);
+		return (encoded.length() - padding) * 3 / 4;
+	}
+
 	static std::string encode(size_t(*func)(char*,const char*,size_t), const std::string &bytes)
 	{
-		size_t encLen = GetEncodedLengthWithoutPadding(bytes.length());
+		size_t encLen = GetEncodedLengthWithPadding(bytes.length());
 		std::string encoded(encLen, '=');
 		func((char*)&encoded[0], (const char*)&bytes[0], bytes.length());
 		return encoded;
+	}
+
+	static std::string decode(size_t(*func)(char* dest, const char* src, size_t len), const std::string &encoded)
+	{
+		size_t decLen = GetDecodedLength(encoded);
+		std::string decoded(decLen, '\0');
+		func((char*)&decoded[0], (const char*)&encoded[0], encoded.length());
+		return decoded;
 	}
 };
 
@@ -31,7 +69,7 @@ struct modp_b64_chromium
 
 	std::string decode(std::string &bytes)
 	{
-		return modp_b64_decode(bytes);
+		return B64_Adapt::decode(modp_b64_decode, bytes);
 	}
 };
 
