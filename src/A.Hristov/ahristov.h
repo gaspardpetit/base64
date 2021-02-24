668a0698ac3b24862089d6c5609017ece1b75195
Alexander Hristov <ahristov@ahristov-pc.prosrm.com>
2021-02-24 21:30:53 +0200

    Adding my implementation

---
+++ src/A.Hristov/ahristov.h
@@ -0,0 +1,10 @@
+#include <string>
+/**
+ * Given a string, this function will encode it in 64b (with padding)
+ */
+std::string encodeBase64(const std::string& binaryText);
+
+/**
+ * Given a 64b padding-encoded string, this function will decode it.
+ */
+std::string decodeBase64(const std::string& base64Text);
